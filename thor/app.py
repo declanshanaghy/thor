f38e18402d225efe88100465fff2b06fd4fcf1b6
Declan Shanaghy <dshanaghy@splunk.com>
2017-09-02 21:07:15 -0700

    Working with electricity organized by channel

--- thor/app.py
+++ thor/app.py
@@ -4,16 +4,11 @@ import datetime
 import json
 import logging
 
-import gem
+import chalicelib.constants as constants
+import chalicelib.seg as seg
 
 app = chalice.Chalice(app_name='thor')
 app.log.setLevel(logging.DEBUG)
-
-REQ_DIR = 'requests'
-LOG_REQUESTS = os.environ.get('LOG_REQUESTS', False)
-if LOG_REQUESTS == 'true':
-    if not os.path.exists(REQ_DIR):
-        os.makedirs(REQ_DIR)
 
 @app.route('/sites/{site}', methods=['PUT'], content_types=[])
 def sites(site):
@@ -21,19 +16,20 @@ def sites(site):
         data = app.current_request.raw_body
         app.log.info("Received data: %s", data)
 
-        if LOG_REQUESTS:
+        if constants.LOG_REQUESTS:
             n = "%s.req" % datetime.datetime.now()
-            p = os.path.join(REQ_DIR, n)
+            p = os.path.join(constants.REQ_DIR, n)
             with open(p, "w") as f:
                 f.write(data)
 
-        g = gem.parse(data)
-        app.log.info("Parsed data: %s", json.dumps(g))
+        g = seg.parse(data)
+        d = json.dumps(g, indent=4, sort_keys=True)
+        app.log.info("Parsed data: %s", d)
         if site == g['site']:
             return g
         else:
             raise chalice.BadRequestError("Requested site does not match data")
     except Exception as ex:
-        app.log.error(str(ex))
+        app.log.error(str(ex), exc_info=True)
         raise chalice.ChaliceViewError(str(ex))
 
